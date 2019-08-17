73707e6d51bf9bbfcde3448385d7c1030b597e87
leoproject <leonardo2013ufs@gmail.com>
2019-08-17 00:28:10 -0300

    Criação de uma SPA com React utilizando API de Terceiro criando a primeiro componente a navbar

---
+++ React/react-dashboard/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MenuSuperior from './components/MenuSuperior/MenuSuperior'
+
+function App() {
+  return (
+    <div className="App">
+      <MenuSuperior/>
+    </div>
+  );
+}
+
+export default App;
