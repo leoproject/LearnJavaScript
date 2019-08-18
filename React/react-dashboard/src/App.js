1ff3ae1b9c264088d3166a880ff0cf97e085fe59
leoproject <leonardo2013ufs@gmail.com>
2019-08-18 15:24:33 -0300

    Criando painel da replicação do código do curso React do DevMedia

--- React/react-dashboard/src/App.js
+++ React/react-dashboard/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import MenuSuperior from './components/MenuSuperior/MenuSuperior'
+import MenuSuperior from './components/MenuSuperior/MenuSuperior';
+import Resumo from './components/Resumo/Resumo';
+
 
 function App() {
   return (
     <div className="App">
       <MenuSuperior/>
+      <Resumo/>
+      
     </div>
   );
 }
