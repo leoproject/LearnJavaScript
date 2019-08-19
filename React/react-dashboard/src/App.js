42a057defedcce7b0959262383689783d6a1fed0
leoproject <leonardo2013ufs@gmail.com>
2019-08-19 10:39:40 -0300

    Criação de esquema de rotas usando react-router-dom --save

--- React/react-dashboard/src/App.js
+++ React/react-dashboard/src/App.js
@@ -2,17 +2,27 @@ import React from 'react';
 import './App.css';
 import MenuSuperior from './components/MenuSuperior/MenuSuperior';
 import Resumo from './components/Resumo/Resumo';
-import Contultas from './components/Consultas/Consultas';
+import Consultas from './components/Consultas/Consultas';
 import Faturamento from './components/Faturamento/Faturamento';
+import { Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <MenuSuperior/>
-      <Resumo/>
-      <Contultas/>
-      <Faturamento/>
+      <MenuSuperior />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <Switch>
+              <Route path="/" exact component={Resumo} />
+              <Route path="/consultas" component={Consultas} />
+              <Route path="/faturamento" component={Faturamento} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
