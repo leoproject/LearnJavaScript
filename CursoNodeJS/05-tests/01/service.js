648bc9b2b6e696b496d9d0302e90852b2cf93f4b
leoproject <leonardo2013ufs@gmail.com>
2019-09-22 23:30:03 +0100

    Introdução a testes automatizados no NodeJS

--- CursoNodeJS/05-tests/01/service.js
+++ CursoNodeJS/05-tests/01/service.js
@@ -6,7 +6,9 @@ const URL = 'https://swapi.co/api/people';
 async function obterPessoas(nome){
     const url =`${URL}/?search=${nome}&format=json`;
     const result = await get(url);
+    
     return result.data.results.map(mapearPessoas);
+    
 }
 
 function mapearPessoas(item){
