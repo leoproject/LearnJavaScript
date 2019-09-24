3021c2beb5a8fb9565a9e6f4ae5c28db7fdc0aa9
leoproject <leonardo2013ufs@gmail.com>
2019-09-24 23:40:36 +0100

    Super da hora criar uma interface no terminal para o projeto

--- CursoNodeJS/06-cli/index.js
+++ CursoNodeJS/06-cli/index.js
@@ -7,16 +7,19 @@ async function main(){
     .version('Versão 0.1')
     .option('-n, --nome [value]',"Nome do Heroi")
     .option('-p, --poder [value]', "Poder do Heroi")
-    .option('-i, --indice [value]', "Indice do Heroi")
+    .option('-i, --id [value]', "Id do Heroi")
 
+    .option('-a, --atualizar [value]', "Atualizar um heroi pelo id")
     .option('-c, --cadastrar', "Cadastrar um heroi")
     .option('-l, --listar', "Listar todos herois")
+    .option('-r, --remover', "Remover heroi pelo id" )
 
     .parse(process.argv)
     const heroi = new Heroi(Commander);
 
     try {
         if (Commander.cadastrar){
+            delete heroi.id;
             const resultado = await Database.cadastrar(heroi);
             if(!resultado){
                 console.error('Heroi não foi cadastrado');
@@ -30,6 +33,29 @@ async function main(){
             const resultado = await Database.listar();
             console.log(resultado);
             return;
+            
+        }
+
+        if(Commander.remover){
+            const resultado = await Database.remover(heroi.id);
+            if (!resultado){
+                console.error('Não foi possível deletar o heroi');
+                return;
+            }
+            console.log('Heroi deletado com êxito!!!!');
+        }
+
+        if(Commander.atualizar){
+            const idParaAtualizar = parseInt(Commander.atualizar);
+            const dado = JSON.stringify(heroi);
+            const heroiAtualizar = JSON.parse(dado);
+            const resultado = await Database.atualizar(idParaAtualizar, heroiAtualizar);
+            
+            if(!resultado){
+                console.error('Não foi possível atualizar!');
+                return;
+            }
+            console.log('Atualizado com sucessso!!!');
             
         }
         
