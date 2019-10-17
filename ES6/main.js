d366ce83e45b7c501970de4e5302faca990beb01
leoproject <leonardo2013ufs@gmail.com>
2019-10-17 23:35:14 +0100

    Aprendendo diferença de var e  const e let

--- ES6/main.js
+++ ES6/main.js
@@ -1,32 +1,24 @@
-class List{
-    constructor(){
-        this.data = [];
-    }
-    add(data){
-        this.data.push(data);
-        console.log(this.data);
-        
+// na const não pode mudar variavel tornando a linguagem fortemente tipada
+
+// const a = 1;
+
+// a = 3;
+
+//mutação é possível dentro de uma variavel de escopo
+const usuario = {nome:'Diego'};
+
+usuario.nome = 'Cleiton'
+
+
+function teste(x){
+    let y = 2;
+    if (x>5){
+        let y=4;
+        console.log(x,y);
     }
 }
 
-class TodoList extends List{
-    constructor(){
-        super();
-        this.usuario = 'Diego';
-    }
-
-    mostrarUsuario(){
-        console.log(this.usuario);
-
-    }
-
-    
-
-}
-
-const MinhaLista = new TodoList();
-
-document.getElementById('novotodo').onclick = function(){
-    MinhaLista.add('Novo TODO POR HERANÇA');
-    MinhaLista.mostrarUsuario();
+teste(10);
+document.getElementById("action").onclick = function(){
+    console.log(usuario);
 }
