93dfb77346e00fdb63585af80c428ef5dffd6954
leoproject <leonardo2013ufs@gmail.com>
2019-10-24 23:37:08 +0100

    Aprendi sobre template literals deixar o codigo de mensagem mais enxuta e elegante

--- ES6/main.js
+++ ES6/main.js
@@ -1,48 +1,6 @@
-//REST 
-
-const usuario ={
-    nome:'Leonardo',
-    idade:27,
-    contato:2344455
-};
-
-const {nome, ...resto} = usuario;
-
-console.log(nome);
-console.log(resto);
-
-const arr = [1,2,3,4];
-const [a,b, ...c] =arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-
-function soma(...params){
-    return params.reduce((total,next)=> total+next);
-}
-
-console.log(soma(2,4,8));
-
-//spread 
-
-const arr1 = [1,2,3];
-const arr2 = [4,5,6];
-
-const arr3 = [...arr1,...arr2];
-console.log(arr3);
-
-const usuario1={
-    nome:'leonardo',
-    idade:27,
-    empresa:'UMINHO'
-};
-
-const usuario2= {...usuario1, nome:'Tico'};
-console.log(usuario2);
-
-
-
-
+const nome = 'Leonardo';
+const idade =27;
+// isso aqui é conhecido como template literals
+// console.log('Meu nome é '+nome+' e tenho '+idade+' anos');
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
 
