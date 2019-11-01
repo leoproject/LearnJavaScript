dcc7c71356707540fcc71fb8a5b73b782d10ed16
leoproject <leonardo2013ufs@gmail.com>
2019-11-01 19:22:17 +0000

    Criei uma aplicaçãozinha irada que busca perfis da API do GitHub pelo nickname do usuário

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -1,20 +1,92 @@
-import axios from 'axios';
+import api from './api';
 
 
-class Api {
-    static async getUserInfo(username) {
-        try {
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            const test = response.data.bio;
-            console.log(response);
-            console.log(test);
-        } catch(error){
-            console.warn('Erro na busca da API');
+class App {
+
+    constructor() {
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    setLoading(loading =true){
+        if(loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createElement.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id','loading');
+
+            this.formEl.appendChild(loadingEl);
         }
-        
-        
+        else {
+            document.getElementById('loading').remove()
+        }
+    }
+
+    async addRepository(event) {
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+        if (repoInput.length === 0) {
+            return;
+        }
+
+        this.setLoading();
+        try {
+            const reponse = await api.get(`/users/${repoInput}`);
+            console.log(response);
+
+            const { name, bio, html_url, avatar_url } = reponse.data;
+            this.repositories.push({
+                name: name,
+                description: bio,
+                avatar_url: avatar_url,
+                html_url: html_url,
+            });
+            this.inputEl.value = '';
+            this.render();
+        } catch (err) {
+            alert(`Repositório ${this.inputEl.value} não existe!!!`);
+
+        }
+        this.setLoading(false);
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url)
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+
+        });
     }
 }
 
-Api.getUserInfo('leoproject');
-Api.getUserInfo('leoproject28312746721');
+new App();
